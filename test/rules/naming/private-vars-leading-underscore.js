08c01a11e1768b6edfd604844ce481394cf6ec90
Miao ZhiCheng <miao@decentral.ee>
2020-06-20 14:04:50 +0300

    implement strict mode for private-vars-leading-underscore instead

--- test/rules/naming/private-vars-leading-underscore.js
+++ test/rules/naming/private-vars-leading-underscore.js
@@ -1,6 +1,6 @@
 const assert = require('assert')
 const linter = require('./../../../lib/index')
-const { contractWith, libraryWith } = require('./../../common/contract-builder')
+const { contractWith, funcWith, libraryWith } = require('./../../common/contract-builder')
 
 describe('Linter - private-vars-leading-underscore', () => {
   const SHOULD_WARN_CASES = [
@@ -79,3 +79,39 @@ describe('Linter - private-vars-leading-underscore', () => {
     })
   })
 })
+
+describe('Linter - private-vars-leading-underscore, strict mode', () => {
+  it('should pass by default', () => {
+    const code = funcWith('uint _b = 1;')
+
+    const report = linter.processStr(code, {
+      rules: {}
+    })
+    assert.equal(report.errorCount, 0)
+  })
+
+  it('should raise error if configured', () => {
+    const code = funcWith('uint _b = 1;')
+    const report = linter.processStr(code, {
+      rules: { 'private-vars-leading-underscore': ['error', { strict: true }] }
+    })
+    assert.equal(report.errorCount, 1)
+  })
+
+  it('should not raise error for non state variable ', () => {
+    const code = contractWith('uint _b = 1;')
+    const report = linter.processStr(code, {
+      rules: { 'private-vars-leading-underscore': ['error', { strict: true }] }
+    })
+    assert.equal(report.errorCount, 0)
+  })
+
+  it('should not raise error for non state mapping variable', () => {
+    const code = contractWith('mapping (address => uint256) internal _balances;')
+    const report = linter.processStr(code, {
+      rules: { 'private-vars-leading-underscore': ['error', { strict: true }] }
+    })
+    assert.equal(report.errorCount, 0)
+  })
+})
+
